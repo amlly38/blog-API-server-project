60051169247099d2d7de044eb38e9338aa99209c
amlly38 <amaliyahamel0308@gmail.com>
2024-11-14 16:27:03 +0700

    feat(server.js): editing server.js

--- src/server.js
+++ src/server.js
@@ -4,4 +4,7 @@ import mongoose from "mongoose";
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/blogDB")
+mongoose.connect('mongodb://localhost:27017/blog-api') 
+    .then(() => app.listen(port))
+    .then(() => console.log(`Server running on port ${port}`))
+    .catch(error => console.log('Connection error', error));
